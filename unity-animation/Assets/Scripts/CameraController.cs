d788495374f8dfe377753d76c9512e92ea57eea0
amenallahg122 <bigpapiamen@gmail.com>
2025-03-14 02:58:51 +0100

    Running in circles

--- unity-animation/Assets/Scripts/CameraController.cs
+++ unity-animation/Assets/Scripts/CameraController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class CameraController : MonoBehaviour 
 {
     public Transform player;
     public float mouseSensitivity = 2f;
@@ -10,34 +10,47 @@ public class CameraController : MonoBehaviour
     public float height = 2f;
     public float rotationSmoothness = 5f;
     public float maxFallDistance = 20f;
-
+    public float playerRotationSpeed = 5f;
+    
     private float rotationY = 0f;
     private float rotationX = 0f;
     private Transform playerTransform;
     private Vector3 startPosition;
     private PlayerMovement playerMovement;
+    private Vector3 movementDirection;
 
-    void Start()
+    void Start() 
     {
         playerTransform = player;
         startPosition = transform.position;
         playerMovement = player.GetComponent<PlayerMovement>();
     }
 
-    void Update()
+    void Update() 
     {
-        if (player.position.y < -maxFallDistance)
+        if (player.position.y < -maxFallDistance) 
         {
-            if (playerMovement != null)
+            if (playerMovement != null) 
             {
                 playerMovement.ResetPlayerPosition();
             }
         }
 
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        
+        movementDirection = new Vector3(horizontalInput, 0, verticalInput).normalized;
+
+        if (movementDirection.magnitude > 0.1f) 
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(movementDirection);
+            player.rotation = Quaternion.Slerp(player.rotation, targetRotation, Time.deltaTime * playerRotationSpeed);
+        }
+
         rotationY += Input.GetAxis("Mouse X") * mouseSensitivity;
         float mouseYInput = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
-        if (PlayerPrefs.GetInt("InvertY", 0) == 1)
+        if (PlayerPrefs.GetInt("InvertY", 0) == 1) 
         {
             mouseYInput = -mouseYInput;
         }
