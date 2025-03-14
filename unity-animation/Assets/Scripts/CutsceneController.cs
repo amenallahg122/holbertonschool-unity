c4b4ed3b2d0669161664bf661a00ccc7e656404c
amenallahg122 <bigpapiamen@gmail.com>
2025-03-14 02:28:46 +0100

    Player

--- unity-animation/Assets/Scripts/CutsceneController.cs
+++ unity-animation/Assets/Scripts/CutsceneController.cs
@@ -1,27 +1,73 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CutsceneController : MonoBehaviour
 {
     public Animator animator;
-    public Camera CutsceneCamera;
-    public Camera MainCamera;
+    public Camera cutsceneCamera;
+    public Camera mainCamera;
+    public GameObject player;
+    public PlayerMovement playerController;
+    public GameObject timerCanvas;
 
     void Start()
     {
-        // Start the cutscene animation
-        animator.Play("Intro01");
-        StartCoroutine(SwitchCameraAfterAnimation());
+        if (animator != null)
+        {
+            if (animator.HasState(0, Animator.StringToHash("Intro01")))
+            {
+                animator.Play("Intro01", 0);
+                if (playerController != null)
+                {
+                    playerController.enabled = false;
+                }
+
+                if (timerCanvas != null)
+                {
+                    timerCanvas.SetActive(false);
+                }
+
+                StartCoroutine(SwitchToGameplay());
+            }
+            else
+            {
+            }
+        }
+        else
+        {
+        }
     }
 
-    private IEnumerator SwitchCameraAfterAnimation()
+    private System.Collections.IEnumerator SwitchToGameplay()
     {
-        // Wait until the animation finishes
-        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        float animationLength = stateInfo.length;
+        Debug.Log("Animation Length: " + animationLength);
 
-        // Switch cameras
-        CutsceneCamera.gameObject.SetActive(false);
-        MainCamera.gameObject.SetActive(true);
+        if (animationLength > 0)
+        {
+            yield return new WaitForSeconds(animationLength);
+        }
+
+        if (mainCamera != null)
+        {
+            mainCamera.gameObject.SetActive(true);
+        }
+
+        if (playerController != null)
+        {
+            playerController.enabled = true;
+        }
+
+        if (timerCanvas != null)
+        {
+            timerCanvas.SetActive(true);
+        }
+
+        if (cutsceneCamera != null)
+        {
+            cutsceneCamera.gameObject.SetActive(false);
+        }
+
+        this.gameObject.SetActive(false);
     }
 }
